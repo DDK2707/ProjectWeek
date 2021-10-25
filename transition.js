f6834ce59ccf1a21b8acdd73c061c809ffae2093
Dale de Kock <ddk2707@gmail.com>
2021-10-25 16:19:17 +0200

    3rd commit, navbar and footer need work

--- transition.js
+++ transition.js
@@ -1,14 +1,9 @@
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
+function openNav() {
+    document.getElementById("mysidebar").style.width = "250px"
+    document.getElementById("main").style.marginLeft = "250px"
+}
 
-for (i = 0; i < dropdown.length; i++) {
-    dropdown [i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        if(dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "none"
-        }
-    });
+function closeNav() {
+    document.getElementById("mysidebar").style.width = "0"
+    document.getElementById("main").style.width = "0"
 }
