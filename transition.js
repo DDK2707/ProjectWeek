5fd316d31531aea55c0df17c116766801a4b1169
Dale de Kock <ddk2707@gmail.com>
2021-10-27 16:22:00 +0200

    27/10, landingbutton, beginning of about section

--- transition.js
+++ transition.js
@@ -1,9 +1,12 @@
 function openNav() {
     document.getElementById("mysidebar").style.width = "250px"
     document.getElementById("main").style.marginLeft = "250px"
+    document.getElementById("openbtn").style.display = "none"
 }
 
 function closeNav() {
     document.getElementById("mysidebar").style.width = "0"
     document.getElementById("main").style.width = "0"
+    document.getElementById("openbtn").style.display = "block"
+    document.getElementById("openbtn").style.marginLeft = "-243px"
 }
