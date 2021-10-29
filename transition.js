1954a7145607b2b5a43b6cae37764fdecfcffc3f
Dale de Kock <ddk2707@gmail.com>
2021-10-29 15:09:29 +0200

    29/10, styling and scroll to top button

--- transition.js
+++ transition.js
@@ -10,3 +10,19 @@ function closeNav() {
     document.getElementById("openbtn").style.display = "block"
     document.getElementById("openbtn").style.marginLeft = "-243px"
 }
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+   document.documentElement.scrollTop = 0;
+  }
