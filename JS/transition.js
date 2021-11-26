aee901229b38559f17c12b1d7df4bb3496d2b77a
Dale <ddk2707@gmail.com>
2021-11-26 19:52:28 +0200

    final resubmission 26/11

---
+++ JS/transition.js
@@ -0,0 +1,28 @@
+function openNav() {
+    document.getElementById("mysidebar").style.width = "250px"
+    document.getElementById("main").style.marginLeft = "250px"
+    document.getElementById("openbtn").style.display = "none"
+}
+
+function closeNav() {
+    document.getElementById("mysidebar").style.width = "0"
+    document.getElementById("main").style.width = "0"
+    document.getElementById("openbtn").style.display = "block"
+    document.getElementById("openbtn").style.marginLeft = "-243px"
+}
+
+mybutton = document.getElementById("myBtn");
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 950 || document.documentElement.scrollTop > 100) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// function topFunction() {
+//    document.documentElement.scrollTop = 0;
+//   }
